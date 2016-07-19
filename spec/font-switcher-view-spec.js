2aed985670f20185b989e2b144e495137f6e66ed
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-07-19 13:55:02 -0500

    initial commit

---
+++ spec/font-switcher-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FontSwitcherView from '../lib/font-switcher-view';
+
+describe('FontSwitcherView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
